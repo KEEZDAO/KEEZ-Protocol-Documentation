c6f2a633d3be84107b0229ed914b7066a750eb43
RobG <rob@keezdao.com>
2022-08-29 18:19:21 -0400

    url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'KEEZ Documentation',
   tagline: 'KEEZ are cool',
-  url: 'https://keezdao.github.io/KEEZ-Protocol-Documentation/',
+  url: 'https://keezdao.github.io/',
   baseUrl: '/KEEZ-Protocol-Documentation/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
