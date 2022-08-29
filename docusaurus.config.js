7fc3226ba1bcf460f1ade0a4febd72d728f4c1f3
RobG <rob@keezdao.com>
2022-08-29 18:23:54 -0400

    config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,7 +34,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          routeBasePath: '/',
+          routeBasePath: '/KEEZ-Protocol-Documentation/',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
